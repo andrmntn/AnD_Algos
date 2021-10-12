984f57d7015844b8f83e9192cffadb5db0fc48d4
Andrmntn <ndrmntn@gmail.com>
2021-10-12 23:30:03 +0200

    Why is eclipse so frickin bad?!

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,12 @@ public class Main {
 
 
         public static void main(String[] args) {
-            int[] input = new int[] { 7, -11, 18, 10, -23, -3, 27, -1};
+            double[] input = new double[] { 1, 0.1, 0.5, 5};
 
-            MaxSubArraySum msas = new MaxSubArraySum(input);
+            //MaxSubArraySum msas = new MaxSubArraySum(input);
             MaxSubArraySumDP msasdp = new MaxSubArraySumDP(input);
 
-            System.out.println(msas.solveMaxSubArraySum(input, 0, input.length-1));
+            //System.out.println(msas.solveMaxSubArraySum(input, 0, input.length-1));
             System.out.println(msasdp.solveMaxSubArraySumDP());
 
         }
